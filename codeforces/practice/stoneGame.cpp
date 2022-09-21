2e8519798d70ec711b3c7cee061df98f720bd665
Jackson Merma <jmermap@unsa.edu.pe>
2022-09-20 23:53:13 -0500

    practica codeforces 20 sep 2022

---
+++ codeforces/practice/stoneGame.cpp
@@ -0,0 +1,82 @@
+/**
+                █▀ █ █▀▄▀█ █▀█
+                ▄█ █ █░▀░█ █▀▀
+ 
+         ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠙⣿⣿⣿⣎⣿⣹⢋⣴⣿⣿⣿⣿⣿⡇⠄⠄⠄
+         ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠈⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⢿⣿⠁⠄⠄⠄
+         ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠂⠈⠛⣿⣿⠿⠿⠋⠉⠁⠄⠙⠛⠄⠄⠄⠄
+         ⠄⠄⠄⠄⠄⢠⣦⣤⣤⣴⣶⡤⠤⣶⣾⣿⣤⣄⡀⣨⣛⡁⠠⣾⡀⠄⠄⠄
+         ⠄⠄⠄⠄⢰⣿⣿⣿⣿⣿⢿⠄⢀⢼⣿⣿⣿⣿⣿⣾⣋⣁⣸⣿⠃⠄⠄⠄
+         ⠄⠄⠄⠄⠄⠍⣿⣿⠟⠁⠐⠄⠃⢸⣿⢋⣿⣿⣿⣿⣿⣿⣿⣿⠄⠄⠄⠄
+         ⠄⠄⠄⠄⠄⠄⡾⠋⠄⠄⠄⠄⠈⣴⣷⠿⣿⠿⣿⣿⣿⣿⣿⠃⠄⠄⠄⠄
+         ⠄⠄⠄⠄⠄⠄⠁⠄⠄⠄⠄⠄⠄⠄⠄⣀⠄⠄⢸⣿⣿⡿⠁⠄⠄⠄⠄⠄
+         ⠄⠄⠄⠄⠄⠄⠄⠄⠄⢀⡆⡄⣬⣣⢿⣾⣶⠄⣿⣿⡿⠁⠄⠄⠄⠄⠄⠄
+         ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠘⠋⠁⡟⣻⠻⣿⠇⠄⣿⠻⠁⠄⠄⠄⠄⠄⠄⠄
+         ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⢰⡇⠄⠄⠄⠄⠄⠄⠄⠄⠄
+         ⠄⠄⠄⠄⠄⠄⠄⠄⠄⡀⠄⠄⠄⠄⢀⡝⢀⡿⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+         ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠱⢠⣀⡀⣤⡾⢁⣿⠃⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define debug(...) fprintf(stderr, __VA_ARGS__), fflush(stderr)
+#define ull unsigned long long
+#define ll long long
+#define ld long double
+#define ulli unsigned long long int
+#define umap unordered_map
+#define uset unordered_set
+#define pqueue priority_queue
+#define pi 3.141592653589793238462
+#define INF 2e9
+#define len length
+#define endl "\n"
+
+void solve(){
+    int n; cin>>n;
+    vector<ll> arr(n);
+
+    for(int i=0;i<n;i++)
+        cin>>arr[i];
+
+    int a=arr[0], b=arr[0];
+    int posA=0, posB=0;
+
+    for(int i=0;i<n;i++){
+        if(arr[i]<a){
+            a=arr[i];
+            posA=i;
+        }
+        if(arr[i]>b){
+            b=arr[i];
+            posB=i;
+        }
+    }
+
+    posA++; posB++;
+    int k1=min(posA, n-posA+1);
+    int k2=min(posB, n-posB+1);
+    if(k1<k2)
+        k2=min(k2, abs(posA-posB));
+    else
+        k1=min(k1, abs(posA-posB));
+    cout<<k1+k2<<endl;
+}
+
+int main(){
+	ios_base::sync_with_stdio(false); cin.tie(NULL);
+    clock_t z = clock();
+
+    ll t; cin>>t;
+
+    while(t--) 
+        solve();
+
+#ifndef ONLINE_JUDGE
+    cout<<endl<<"Tiempo total: "<<fixed<<setprecision(3)<<(double)(clock()-z)/CLOCKS_PER_SEC<<endl;
+#endif
+
+	return 0;
+}
+
