cc15fc197e99279de7efe647d2e1def8f281ecc2
Jackson Merma <jmermap@unsa.edu.pe>
2022-09-28 00:15:38 -0500

    practica codeforces 28 sep 2022

---
+++ codeforces/practice/computerGame.cpp
@@ -0,0 +1,71 @@
+/**
+                █▀ █ █▀▄▀█ █▀█
+                ▄█ █ █░▀░█ █▀▀
+ 
+         ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠙⣿⣿⣿⣎⣿⣹⢋⣴⣿⣿⣿⣿⣿⡇⠄⠄⠄
+         ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠈⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⢿⣿⠁⠄⠄⠄
+         ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠂⠈⠛⣿⣿⠿⠿⠋⠉⠁⠄⠙⠛⠄⠄⠄⠄
+         ⠄⠄⠄⠄⠄⢠⣦⣤⣤⣴⣶⡤⠤⣶⣾⣿⣤⣄⡀⣨⣛⡁⠠⣾⡀⠄⠄⠄
+         ⠄⠄⠄⠄⢰⣿⣿⣿⣿⣿⢿⠄⢀⢼⣿⣿⣿⣿⣿⣾⣋⣁⣸⣿⠃⠄⠄⠄
+         ⠄⠄⠄⠄⠄⠍⣿⣿⠟⠁⠐⠄⠃⢸⣿⢋⣿⣿⣿⣿⣿⣿⣿⣿⠄⠄⠄⠄
+         ⠄⠄⠄⠄⠄⠄⡾⠋⠄⠄⠄⠄⠈⣴⣷⠿⣿⠿⣿⣿⣿⣿⣿⠃⠄⠄⠄⠄
+         ⠄⠄⠄⠄⠄⠄⠁⠄⠄⠄⠄⠄⠄⠄⠄⣀⠄⠄⢸⣿⣿⡿⠁⠄⠄⠄⠄⠄
+         ⠄⠄⠄⠄⠄⠄⠄⠄⠄⢀⡆⡄⣬⣣⢿⣾⣶⠄⣿⣿⡿⠁⠄⠄⠄⠄⠄⠄
+         ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠘⠋⠁⡟⣻⠻⣿⠇⠄⣿⠻⠁⠄⠄⠄⠄⠄⠄⠄
+         ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⢰⡇⠄⠄⠄⠄⠄⠄⠄⠄⠄
+         ⠄⠄⠄⠄⠄⠄⠄⠄⠄⡀⠄⠄⠄⠄⢀⡝⢀⡿⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+         ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠱⢠⣀⡀⣤⡾⢁⣿⠃⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define debug(...) fprintf(stderr, __VA_ARGS__), fflush(stderr)
+#define ull unsigned long long
+#define ll long long
+#define ld long double
+#define ulli unsigned long long int
+#define umap unordered_map
+#define uset unordered_set
+#define pqueue priority_queue
+#define pi 3.141592653589793238462
+#define INF 2e9
+#define len length
+#define endl "\n"
+
+void solve(){
+    int n; cin>>n;
+    string a, b; cin>>a>>b;
+    int dp[2][n];
+    dp[0][0]=a[0]-'0';
+    dp[1][0]=(!(b[0]-'0'))?dp[0][0]:b[0]-'0';
+
+    for(int i=1;i<n;i++){
+        dp[0][i]=a[i]-'0';
+        dp[1][i]=b[i]-'0';
+
+        if(!(dp[0][i]))
+            dp[0][i]=min(dp[0][i-1], dp[1][i-1]);
+        if(!(dp[1][i]))
+            dp[1][i]=min(dp[1][i-1], dp[0][i-1]);
+    }
+    if(dp[1][n-1]) cout<<"NO"<<endl;
+    else cout<<"YES"<<endl;
+}
+
+int main(){
+	ios_base::sync_with_stdio(false); cin.tie(NULL);
+    clock_t z = clock();
+
+    ll t; cin>>t;
+
+    while(t--) 
+        solve();
+
+#ifndef ONLINE_JUDGE
+    cout<<endl<<"Tiempo total: "<<fixed<<setprecision(3)<<(double)(clock()-z)/CLOCKS_PER_SEC<<endl;
+#endif
+
+	return 0;
+}
+
---
+++ codeforces/practice/evenSubsetSumProblem.cpp
@@ -0,0 +1,86 @@
+/**
+                █▀ █ █▀▄▀█ █▀█
+                ▄█ █ █░▀░█ █▀▀
+ 
+         ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠙⣿⣿⣿⣎⣿⣹⢋⣴⣿⣿⣿⣿⣿⡇⠄⠄⠄
+         ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠈⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⢿⣿⠁⠄⠄⠄
+         ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠂⠈⠛⣿⣿⠿⠿⠋⠉⠁⠄⠙⠛⠄⠄⠄⠄
+         ⠄⠄⠄⠄⠄⢠⣦⣤⣤⣴⣶⡤⠤⣶⣾⣿⣤⣄⡀⣨⣛⡁⠠⣾⡀⠄⠄⠄
+         ⠄⠄⠄⠄⢰⣿⣿⣿⣿⣿⢿⠄⢀⢼⣿⣿⣿⣿⣿⣾⣋⣁⣸⣿⠃⠄⠄⠄
+         ⠄⠄⠄⠄⠄⠍⣿⣿⠟⠁⠐⠄⠃⢸⣿⢋⣿⣿⣿⣿⣿⣿⣿⣿⠄⠄⠄⠄
+         ⠄⠄⠄⠄⠄⠄⡾⠋⠄⠄⠄⠄⠈⣴⣷⠿⣿⠿⣿⣿⣿⣿⣿⠃⠄⠄⠄⠄
+         ⠄⠄⠄⠄⠄⠄⠁⠄⠄⠄⠄⠄⠄⠄⠄⣀⠄⠄⢸⣿⣿⡿⠁⠄⠄⠄⠄⠄
+         ⠄⠄⠄⠄⠄⠄⠄⠄⠄⢀⡆⡄⣬⣣⢿⣾⣶⠄⣿⣿⡿⠁⠄⠄⠄⠄⠄⠄
+         ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠘⠋⠁⡟⣻⠻⣿⠇⠄⣿⠻⠁⠄⠄⠄⠄⠄⠄⠄
+         ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⢰⡇⠄⠄⠄⠄⠄⠄⠄⠄⠄
+         ⠄⠄⠄⠄⠄⠄⠄⠄⠄⡀⠄⠄⠄⠄⢀⡝⢀⡿⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+         ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠱⢠⣀⡀⣤⡾⢁⣿⠃⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define debug(...) fprintf(stderr, __VA_ARGS__), fflush(stderr)
+#define ull unsigned long long
+#define ll long long
+#define ld long double
+#define ulli unsigned long long int
+#define umap unordered_map
+#define uset unordered_set
+#define pqueue priority_queue
+#define pi 3.141592653589793238462
+#define INF 2e9
+#define len length
+#define endl "\n"
+
+void solve(){
+    int n; cin>>n;
+    int arr[n];
+
+    for(int i=0;i<n;i++)
+        cin>>arr[i];
+
+    int ci=0;
+
+    for(int i=0;i<n;i++){
+        if(!(arr[i]%2)){
+            cout<<1<<endl;
+            cout<<i+1<<endl;
+            return;
+        }else{
+            ci++;
+        }
+    }
+    if(ci<2){
+        cout<<-1<<endl;
+        return;
+    }
+    cout<<2<<endl;
+    for(int i=0;i<n;i++){
+        if(arr[i]%2){
+            cout<<i+1<<" ";
+            ci--;
+            if(!(ci)){
+                cout<<endl;
+                return;
+            }
+        }
+    }
+}
+
+int main(){
+	ios_base::sync_with_stdio(false); cin.tie(NULL);
+    clock_t z = clock();
+
+    ll t; cin>>t;
+
+    while(t--) 
+        solve();
+
+#ifndef ONLINE_JUDGE
+    cout<<endl<<"Tiempo total: "<<fixed<<setprecision(3)<<(double)(clock()-z)/CLOCKS_PER_SEC<<endl;
+#endif
+
+	return 0;
+}
+
